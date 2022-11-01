ec421125f1bf4a894c21b3b782dea4d26f774eac
Thomas Biasini <thomas.biasini@etu.minesparis.psl.eu>
2022-11-01 22:42:01 +0100

    v1

--- Snake.py
+++ Snake.py
@@ -1,33 +1,37 @@
-# v0 : on repeint l'écran à une période de 1 seconde
-# et on a du mal à sortir du programme
+# v1 : pareil mais au moins on peut sortir du programme
+# avec la touche 'q', ou avec la souris en fermant la fenêtre
 
-# les imports standard en premier
 from random import randint
-
 import pygame as pg
 
-# on initialise pygame et on crée une fenêtre de 400x300 pixels
 pg.init()
 screen = pg.display.set_mode((400, 300))
-
-# on crée aussi un objet "horloge"
 clock = pg.time.Clock()
 
-# enfin on boucle à l'infini pour faire le rendu de chaque image
-while True:
-    # l'objet "clock" permet de limiter le nombre d'images par secondes
-    # ici pour cette démo on demande 1 image par seconde
+# on rajoute une condition à la boucle: si on la passe à False le programme s'arrête
+running = True
+while running:
+
     clock.tick(1)
 
-    # il faut traiter les événements a minima
-    # pour que la fenêtre s'affiche
-    for event in pg.event.get():
-        pass
-
-    # on génère une couleur (Rouge, Vert, Bleu) au hasard
     random_color = (randint(0, 255), randint(0, 255), randint(0, 255))
-    # et on colorie l'écran avec cette couleur
     screen.fill(random_color)
-
-    # enfin on met à jour la fenêtre avec tous les changements
     pg.display.update()
+
+    # on itère sur tous les évênements qui ont eu lieu depuis le précédent appel
+    # ici donc tous les évènements survenus durant la seconde précédente
+    for event in pg.event.get():
+        # chaque évênement à un type qui décrit la nature de l'évênement
+        # un type de pg.QUIT signifie que l'on a cliqué sur la "croix" de la fenêtre
+        if event.type == pg.QUIT:
+            running = False
+        # un type de pg.KEYDOWN signifie que l'on a appuyé une touche du clavier
+        elif event.type == pg.KEYDOWN:
+            # si la touche est "Q" on veut quitter le programme
+            if event.key == pg.K_q:
+                running = False
+
+
+# Enfin on rajoute un appel à pg.quit()
+# Cet appel va permettre à Pygame de "bien s'éteindre" et éviter des bugs sous Windows
+pg.quit()
