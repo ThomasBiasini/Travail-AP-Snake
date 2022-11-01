bd990409b18401052c9c607727254a72a143b8c4
Thomas Biasini <thomas.biasini@etu.minesparis.psl.eu>
2022-11-01 22:41:29 +0100

    v1

--- Snake.py
+++ Snake.py
@@ -1,1 +1,33 @@
-#hello
+# v0 : on repeint l'écran à une période de 1 seconde
+# et on a du mal à sortir du programme
+
+# les imports standard en premier
+from random import randint
+
+import pygame as pg
+
+# on initialise pygame et on crée une fenêtre de 400x300 pixels
+pg.init()
+screen = pg.display.set_mode((400, 300))
+
+# on crée aussi un objet "horloge"
+clock = pg.time.Clock()
+
+# enfin on boucle à l'infini pour faire le rendu de chaque image
+while True:
+    # l'objet "clock" permet de limiter le nombre d'images par secondes
+    # ici pour cette démo on demande 1 image par seconde
+    clock.tick(1)
+
+    # il faut traiter les événements a minima
+    # pour que la fenêtre s'affiche
+    for event in pg.event.get():
+        pass
+
+    # on génère une couleur (Rouge, Vert, Bleu) au hasard
+    random_color = (randint(0, 255), randint(0, 255), randint(0, 255))
+    # et on colorie l'écran avec cette couleur
+    screen.fill(random_color)
+
+    # enfin on met à jour la fenêtre avec tous les changements
+    pg.display.update()
