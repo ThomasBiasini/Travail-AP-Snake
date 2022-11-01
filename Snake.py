26a25f4e1780a4a941dd5e96776d2edf257d9ed1
Thomas Biasini <thomas.biasini@etu.minesparis.psl.eu>
2022-11-01 23:07:55 +0100

    damier

--- Snake.py
+++ Snake.py
@@ -1,22 +1,28 @@
-<<<<<<< HEAD
-# v1 : pareil mais au moins on peut sortir du programme
-# avec la touche 'q', ou avec la souris en fermant la fenêtre
 
 from random import randint
 import pygame as pg
 
 pg.init()
-screen = pg.display.set_mode((400, 300))
+screen = pg.display.set_mode((600, 600))
 clock = pg.time.Clock()
+for i in range(15) :
+    for j in range(30) :
+        # les coordonnées de rectangle que l'on dessine
+        x = 40*i+20*(j%2) # coordonnée x (colonnes) en pixels
+        y = 20*j # coordonnée y (lignes) en pixels
+        width = 20 # largeur du rectangle en pixels
+        height = 20 # hauteur du rectangle en pixels
+        rect = pg.Rect(x, y, width, height)
+        # appel à la méthode draw.rect()
+        color = (255, 0, 0) # couleur rouge
+        pg.draw.rect(screen, color, rect)
 
 # on rajoute une condition à la boucle: si on la passe à False le programme s'arrête
 running = True
 while running:
 
     clock.tick(1)
-
-    random_color = (randint(0, 255), randint(0, 255), randint(0, 255))
-    screen.fill(random_color)
+    
     pg.display.update()
 
     # on itère sur tous les évênements qui ont eu lieu depuis le précédent appel
@@ -36,38 +42,3 @@ while running:
 # Enfin on rajoute un appel à pg.quit()
 # Cet appel va permettre à Pygame de "bien s'éteindre" et éviter des bugs sous Windows
 pg.quit()
-=======
-# v0 : on repeint l'écran à une période de 1 seconde
-# et on a du mal à sortir du programme
-
-# les imports standard en premier
-from random import randint
-
-import pygame as pg
-
-# on initialise pygame et on crée une fenêtre de 400x300 pixels
-pg.init()
-screen = pg.display.set_mode((400, 300))
-
-# on crée aussi un objet "horloge"
-clock = pg.time.Clock()
-
-# enfin on boucle à l'infini pour faire le rendu de chaque image
-while True:
-    # l'objet "clock" permet de limiter le nombre d'images par secondes
-    # ici pour cette démo on demande 1 image par seconde
-    clock.tick(1)
-
-    # il faut traiter les événements a minima
-    # pour que la fenêtre s'affiche
-    for event in pg.event.get():
-        pass
-
-    # on génère une couleur (Rouge, Vert, Bleu) au hasard
-    random_color = (randint(0, 255), randint(0, 255), randint(0, 255))
-    # et on colorie l'écran avec cette couleur
-    screen.fill(random_color)
-
-    # enfin on met à jour la fenêtre avec tous les changements
-    pg.display.update()
->>>>>>> 812fbeded2796afae4a37c2283ebc4ed3ea83229
